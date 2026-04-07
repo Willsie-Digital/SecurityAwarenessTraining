d5bcb6629ff6af9035ab02a31ed47f7ad2c3e22e
Todd Willsie <todd@willsie.net>
2026-04-07 17:13:07 -0600

    fixed syntax error

--- sat.ps1
+++ sat.ps1
@@ -31,15 +31,20 @@ Set-ItemProperty -Path $RegPath -Name TileWallpaper   -Value $Style.Tile
 Set-ItemProperty -Path $RegPath -Name Wallpaper       -Value $LocalPath
 
 
-# --- REFRESH WALLPAPER ---
+
+# --- APPLY WALLPAPER IMMEDIATELY ---
 Add-Type @"
-using System;
 using System.Runtime.InteropServices;
-public class NativeMethods {
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+public class Wallpaper {
+    [DllImport("user32.dll", SetLastError=true)]
+    public static extern bool SystemParametersInfo(
+        int uAction,
+        int uParam,
+        string lpvParam,
+        int fuWinIni
+    );
 }
 "@
 
-# Apply immediately
-:SystemParametersInfo(20, 0, $LocalPath, 3)
+[Wallpaper]::SystemParametersInfo(20, 0, $LocalPath, 3) | Out-Null
+
